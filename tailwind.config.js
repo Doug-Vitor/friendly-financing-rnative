13afb4875867af37e3fdc263394eb52bc7b13293
Douglas Vitor <douglasvitor.dev@gmail.com>
2025-03-01 21:30:26 -0300

    Adding identity colors. May change (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#8bff00',
+          dark: '#5ed600',
+        },
+        secondary: {
+          light: '#008900',
+          dark: '#005400',
+        },
+        typography: {
+          light: '#212121',
+          dark: '#fff',
+        },
+        background: {
+          light: '#fff',
+          dark: '#000',
+        },
+      },
+    },
   },
   plugins: [],
 };
